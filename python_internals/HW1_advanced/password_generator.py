b3cf6c1ff887377e6ee9e9e57831f95926937740
Ihor Aryku <igorarycu@gmail.com>
2023-06-30 11:05:35 +0300

    Create password_generator.py v1

--- python_internals/HW1_advanced/password_generator.py
+++ python_internals/HW1_advanced/password_generator.py
@@ -1,5 +1,17 @@
 import random
+from string import printable
+
+print("Welcome to the Linux User Password Generator!\n")
 
 
-def generator():
-    pass
+def pas_generate():
+    password_length = int(input("Please enter the desired password length: "))
+    password = ""
+
+    for _ in range(password_length):
+        password += random.choice(list(printable))
+
+    print(f"\nGenerated password: {password}")
+
+
+pas_generate()
