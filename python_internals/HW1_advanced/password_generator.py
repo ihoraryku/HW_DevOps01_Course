5199877f3e4771d50c0f56d1937f92f18cdc5ec3
Ihor Aryku <igorarycu@gmail.com>
2023-06-30 11:58:20 +0300

    Upgrade password_generator. py to v3. Added try-except if the password length is less than 4

--- python_internals/HW1_advanced/password_generator.py
+++ python_internals/HW1_advanced/password_generator.py
@@ -5,18 +5,24 @@ print("Welcome to the Linux User Password Generator!\n")
 
 
 def pas_generate():
-    password_length = int(input("Please enter the desired password length: "))
-    password = [random.choice(ascii_lowercase), random.choice(ascii_uppercase), random.choice(digits),
-                random.choice(punctuation)]
+    try:
+        password_length = int(input("Please enter the desired password length: "))
+        if password_length < 4:
+            raise ValueError("Error: The password length cannot be less than 4. Please try again.")
+        password = [random.choice(ascii_lowercase), random.choice(ascii_uppercase), random.choice(digits),
+                    random.choice(punctuation)]
 
-    length_over_4 = password_length - 4
+        length_over_4 = password_length - 4
 
-    for _ in range(length_over_4):
-        password.append(random.choice(ascii_lowercase + ascii_uppercase + digits + punctuation))
+        for _ in range(length_over_4):
+            password.append(random.choice(ascii_lowercase + ascii_uppercase + digits + punctuation))
 
-    password = ''.join(password)
+        password = ''.join(password)
 
-    print(f"\nGenerated password: {password}")
+        print(f"\nGenerated password: {password}")
+
+    except ValueError as e:
+        print(e)
 
 
 pas_generate()
