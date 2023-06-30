68d9eceb38816a5d0288803c0ca25d1be51c3b2b
Ihor Aryku <igorarycu@gmail.com>
2023-06-30 11:43:03 +0300

    Upgrade password_generator.py to v2

--- python_internals/HW1_advanced/password_generator.py
+++ python_internals/HW1_advanced/password_generator.py
@@ -1,15 +1,20 @@
 import random
-from string import printable
+from string import ascii_lowercase, ascii_uppercase, digits, punctuation
 
 print("Welcome to the Linux User Password Generator!\n")
 
 
 def pas_generate():
     password_length = int(input("Please enter the desired password length: "))
-    password = ""
+    password = [random.choice(ascii_lowercase), random.choice(ascii_uppercase), random.choice(digits),
+                random.choice(punctuation)]
 
-    for _ in range(password_length):
-        password += random.choice(list(printable))
+    length_over_4 = password_length - 4
+
+    for _ in range(length_over_4):
+        password.append(random.choice(ascii_lowercase + ascii_uppercase + digits + punctuation))
+
+    password = ''.join(password)
 
     print(f"\nGenerated password: {password}")
 
