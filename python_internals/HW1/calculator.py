1f92ef27a27789061fb013156a20ce071d89fd50
Ihor Aryku <igorarycu@gmail.com>
2023-07-04 17:09:44 +0300

    Refactored calculator.py

--- python_internals/HW1/calculator.py
+++ python_internals/HW1/calculator.py
@@ -1,41 +1,91 @@
+#!/usr/bin/env python
+
+
+MAX_ATTEMPTS = 3
+DECIMAL_PRECISION = None
+
 
 def calculate():
     print("Welcome to the Calculator Program!\n")
 
-    num1 = int(input("Please enter the first number: "))
-    num2 = int(input("Please enter the second number: "))
-    operator = input(
-        """\nPlease select an operation:
-        1. Addition
-        2. Subtraction
-        3. Multiplication
-        4. Division
+    num1, num2 = get_numbers()
+    operator = get_operator()
+    result = perform_operation(num1, num2, operator)
+    print_result(result)
 
-        Enter your choice (1-4): """
-    )
 
-    try:
-        operator = {
-            "1": "+",
-            "2": "-",
-            "3": "*",
-            "4": "/"
-        }[operator]
-    except KeyError:
-        print("\nInvalid choice. Please try again.")
-        return
+def get_numbers():
+    def get_float_input(message):
+        for attempts in range(MAX_ATTEMPTS):
+            try:
+                value = float(input(message).replace(',', '.'))
+                return value
+            except ValueError:
+                print("\nInvalid value. Please enter an integer or float number!\n")
 
-    if operator == "/" and num2 == 0:
-        print("\nError: Cannot divide by zero.")
-        return
+        print(f"You entered the wrong value {MAX_ATTEMPTS} times. Please check your initial data and try again later.")
+        exit(0)
 
+    num1 = get_float_input("Please enter the first number: ")
+    num2 = get_float_input("Please enter the second number: ")
+    return num1, num2
+
+
+def get_operator():
+    for attempts in range(MAX_ATTEMPTS):
+        try:
+            operator = input(
+                """\nPlease select an operation:
+                1. Addition
+                2. Subtraction
+                3. Multiplication
+                4. Division
+
+                Enter your choice (1-4): """
+            )
+
+            operator = {
+                "1": "+",
+                "2": "-",
+                "3": "*",
+                "4": "/"
+            }[operator]
+
+            return operator
+
+        except KeyError:
+            print("\nInvalid choice. Please enter an integer between 1 and 4!")
+
+    print(f"You entered the wrong value {MAX_ATTEMPTS} times. Please check your initial data and try again later.")
+    exit(0)
+
+
+def perform_operation(num1, num2, operator):
     try:
         result = eval(f"{num1} {operator} {num2}")
-        print("\nThe result is:", result)
+        return result
     except ZeroDivisionError:
-        print("\nError: Cannot divide by zero.")
-    except Exception as e:
-        print(f"\nAn error occurred: {str(e)}")
+        print("\nError: Cannot divide by zero. \nPlease select another value or operation and try again!")
+        return None
+    except Exception as error:
+        print(f"\nAn error occurred: {str(error)}")
+        return None
+
+
+def print_result(result):
+    if result is not None:
+        result = format_result(result)
+        print("\nThe result is:", result)
+
+
+def format_result(result):
+    if DECIMAL_PRECISION is not None:
+        if result == int(result):
+            return int(result)
+        else:
+            return round(result, DECIMAL_PRECISION)
+    else:
+        return result
 
 
 calculate()
